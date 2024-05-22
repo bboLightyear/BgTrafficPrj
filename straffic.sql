6c7b2787c67588eb8cf280d433acbe910d655872
bboLightyear <158018897+bboLightyear@users.noreply.github.com>
2024-05-23 00:52:20 +0900

    traffic prj

---
+++ straffic.sql
@@ -0,0 +1,115 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 월요일-5월-13-2024   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table STRAFFICNOTICE
+--------------------------------------------------------
+
+  CREATE TABLE "HR"."STRAFFICNOTICE" 
+   (	"SNO" NUMBER, 
+	"STITLE" VARCHAR2(300 BYTE), 
+	"SCONTENT" VARCHAR2(3000 BYTE), 
+	"SDATE" DATE, 
+	"SHIT" NUMBER DEFAULT 0, 
+	"SFILESRC" VARCHAR2(100 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into HR.STRAFFICNOTICE
+SET DEFINE OFF;
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (14,'ㄴㄴ','ㄴㄴ',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (15,'ㄴㄴ','ㄴㄴ',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (11,'ㅇㅇ','ㅇㅇ',to_date('24/05/13','RR/MM/DD'),0,'1715572384988_조보근-민증.jpg');
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (12,'[안내] 5/19(일) 2024 서울자전거대행진 행사에 따른 통제 안내','「2024 차 없는 잠수교 뚜벅뚜벅 축제」와 관련하여 아래와 같이 교통통제가 실시됩니다. 
+
+행사 주변 도로 혼잡이 우려되므로, 대중교통 이용 부탁드립니다.
+
+ 
+
+□ 행사 및 통제개요
+
+
+
+○ 행사명 : 2024 차 없는 잠수교 뚜벅뚜벅 축제
+
+- 행사장소 : 잠수교 남단 달빛광장 일원(반포한강공원)
+
+- 행사기간 : (매주 일요일) 상반기 5.5 ~ 6.23.  * 5/5(일) 우천 취소
+
+                             하반기 9.1.~10.27.  * 추석연휴 제외  
+
+- 행사시간 : (행사) 13:00 ~ 21:00
+
+ 
+
+ 
+
+○ 현장 교통 통제 
+
+- 통제시간 : 11:00 ~ 23:00
+
+- 통제구간 : 잠수교 북단 ~ 남단 달빛광장 (L=1.1km)
+
+  
+
+
+※ 자세한 내용은 첨부파일을 참고 부탁드립니다.',to_date('24/05/13','RR/MM/DD'),0,'1715581015471_crystal-ball-4006971.jpg');
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (13,'공지사항입니다1 (수정)','ㄴㄴㄴ',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (1,'공지사항입니다1 (수정)','							공지사항입니다1
+						',to_date('24/05/10','RR/MM/DD'),0,'1715588323435_lion.png');
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (2,'공지사항입니다1','공지사항입니다1',to_date('24/05/10','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (3,'공지사항입니다1','공지사항입니다1',to_date('24/05/10','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (4,'공지사항입니다1','공지사항입니다1',to_date('24/05/10','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (5,'공지사항입니다1','공지사항입니다1',to_date('24/05/10','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (6,'공지사항입니다1','공지사항입니다1',to_date('24/05/10','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (7,'공지사항입니다1','공지사항입니다1',to_date('24/05/10','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (16,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (17,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (18,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (19,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (20,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (21,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (22,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (23,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (24,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (25,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (26,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (27,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (28,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (29,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (30,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (31,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (32,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (33,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (34,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (35,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (36,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (37,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (38,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (39,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (40,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (41,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (42,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (43,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (44,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+Insert into HR.STRAFFICNOTICE (SNO,STITLE,SCONTENT,SDATE,SHIT,SFILESRC) values (45,'공지사항입니다1','공지사항입니다1',to_date('24/05/13','RR/MM/DD'),0,null);
+--------------------------------------------------------
+--  DDL for Index SYS_C007211
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "HR"."SYS_C007211" ON "HR"."STRAFFICNOTICE" ("SNO") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  Constraints for Table STRAFFICNOTICE
+--------------------------------------------------------
+
+  ALTER TABLE "HR"."STRAFFICNOTICE" ADD PRIMARY KEY ("SNO")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS"  ENABLE;
